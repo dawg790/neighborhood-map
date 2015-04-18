daecee0bf00518dc487a6d9a50f6d3f1b337c1e1
Nick Woodland <nick_woodland@hotmail.com>
2015-04-17 22:56:19 -0600

    First version pulling places data from Foursquare

--- src/js/app.js
+++ src/js/app.js
@@ -51,7 +51,6 @@ var allSettings = [
 
 // This is the proper model format for data,
 var Model = function(data) {
-
 	// AJAX request here???
 };
 
@@ -59,26 +58,19 @@ var ViewModel = function() {
 	var self = this;
 	self.map = null;
 
-	// Creating an empy observable array for the Places
-	self.allPlaces = ko.observableArray([]);
-
-	// Making all the places we need and putting them in the allPlaces array
-	places.forEach(function(markerItem) {
-		self.allPlaces.push(markerItem);
-	});
-
 	// Click event for the list view
 	self.selectPlace = function(clickedItem) {
 		// Cycle through each marker, set animation to null so that with each click, the markers reset
-		for (var i = 0; i < self.allPlaces().length; i++) {
-			self.allPlaces()[i].marker.setAnimation(null)
+		for (var i = 0; i < self.fsPlaces().length; i++) {
+			self.fsPlaces()[i].marker.setAnimation(null)
 		};
 		// Set the clicked marker to Bounce
 		clickedItem.marker.setAnimation(google.maps.Animation.BOUNCE);
 		// TODO: Set the other infowindows to close on click
-		clickedItem.infowindow.open(self.map, this.marker);
+		// clickedItem.infowindow.open(self.map, this.marker);
 		// This centers the map on the selected place
-		self.map.setCenter(new google.maps.LatLng(clickedItem.lat, clickedItem.long));
+		self.map.setCenter(new google.maps.LatLng(clickedItem.location.lat, clickedItem.location.lng));
+
 	};
 
 	// Click event for Map settings options
@@ -92,18 +84,29 @@ var ViewModel = function() {
 		}
 	};
 
-	// AJAX Request to Foursquare
-	// TODO: Reset the app to pull all places from the this array instead of the object literal above
-	self.foursquarePlaces = ko.observableArray([]);
-
-	var city = "Breckenridge,CO";
-	$.getJSON("https://api.foursquare.com/v2/venues/search?client_id=MUKBUW43YPMWUS2HKDZQZW4VYLT5B1HHST20VR5K35WAKFVC&client_secret=5I1RMLBOLDC1QXU5IJN4VLC2E1N2G1JIGB3QUG5FTAZO4CFM&v=20130815&near=" + city, function(data) {
-    	for (var i = 0; i < data.response.venues.length; i++) {
-    		self.foursquarePlaces.push(data.response.venues[i]);
-    	};
-  });
+	// Define ko array for our AJAX request
+	self.fsPlaces = ko.observableArray([]);
 
 	self.initializeMap = function() {
+		// TODO: Hardcoding city should be removed and come from either input or settings
+		var city = "Breckenridge,CO";
+		$.getJSON("https://api.foursquare.com/v2/venues/search?client_id=MUKBUW43YPMWUS2HKDZQZW4VYLT5B1HHST20VR5K35WAKFVC&client_secret=5I1RMLBOLDC1QXU5IJN4VLC2E1N2G1JIGB3QUG5FTAZO4CFM&v=20130815&near=" + city, function(data) {
+		  self.fsPlaces(data.response.venues);
+		  // This function within the request, allows us to use the data outside this scope.
+		  setMarkers();
+		});
+		// Function is called above and places the markers into the fsPlaces array.
+		function setMarkers() {
+			for (var i = 0; i < self.fsPlaces().length; i++) {
+				self.fsPlaces()[i].marker = new google.maps.Marker({
+				  position: new google.maps.LatLng(self.fsPlaces()[i].location.lat, self.fsPlaces()[i].location.lng),
+				  title: self.fsPlaces()[i].name,
+				  map: self.map,
+				  animation: google.maps.Animation.DROP,
+				  // TODO: need the InfoWindows now
+			  });
+			}
+		}
 		// Using Google's Styled Map options. JSON below was built with Styled Map Wizard
 		var styles = [
 		  {"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#1080dd"}]},
@@ -130,27 +133,27 @@ var ViewModel = function() {
 		bikeLayer.setMap(self.map);
 
 		// Add Markers & Infowindows
-		for (var i = 0; i < self.allPlaces().length; i++) {
-			self.allPlaces()[i].marker = new google.maps.Marker({
-			  position: new google.maps.LatLng(self.allPlaces()[i].lat, self.allPlaces()[i].long),
-			  title: self.allPlaces()[i].title,
-			  map: self.map,
-			  animation: google.maps.Animation.DROP,
-			  html: self.allPlaces()[i].content,
-			  infowindow: self.allPlaces()[i].infowindow
-		  });
+		// for (var i = 0; i < self.allPlaces().length; i++) {
+		// 	self.allPlaces()[i].marker = new google.maps.Marker({
+		// 	  position: new google.maps.LatLng(self.allPlaces()[i].lat, self.allPlaces()[i].long),
+		// 	  title: self.allPlaces()[i].title,
+		// 	  map: self.map,
+		// 	  animation: google.maps.Animation.DROP,
+		// 	  html: self.allPlaces()[i].content,
+		// 	  infowindow: self.allPlaces()[i].infowindow
+		//   });
 
-		  self.allPlaces()[i].infowindow = new google.maps.InfoWindow({
-  		  content: self.allPlaces()[i].marker.html
-		  });
-		  // Utilizing a closure here to add event listeners to each Marker
-		  // TODO: Need a close() event to close other infowindows on click
-		  google.maps.event.addListener(self.allPlaces()[i].marker, 'click', (function(innerKey) {
-        return function() {
-        	self.allPlaces()[innerKey].infowindow.open(self.map, self.allPlaces()[innerKey].marker);
-        }
-      })(i));
-		}
+		//   self.allPlaces()[i].infowindow = new google.maps.InfoWindow({
+  // 		  content: self.allPlaces()[i].marker.html
+		//   });
+		//   // Utilizing a closure here to add event listeners to each Marker
+		//   // TODO: Need a close() event to close other infowindows on click
+		//   google.maps.event.addListener(self.allPlaces()[i].marker, 'click', (function(innerKey) {
+  //       return function() {
+  //       	self.allPlaces()[innerKey].infowindow.open(self.map, self.allPlaces()[innerKey].marker);
+  //       }
+  //     })(i));
+		// }
 
 		// Calling setOptions on the map and setting it to the styles array from above
 		self.map.setOptions({styles: styles});
